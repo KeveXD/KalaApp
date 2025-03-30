4588b473c8cd4163f7b4606d0a952ece4c1daba4
Keve <keve.balla@gmail.com>
2025-03-30 23:59:48 +0200

    raktaaar

--- KalaApp/lib/svg/jobb_raktar_svg.dart
+++ KalaApp/lib/svg/jobb_raktar_svg.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../constants.dart';
 
 class JobbRaktarSvg extends StatefulWidget {
   @override
@@ -10,136 +7,82 @@ class JobbRaktarSvg extends StatefulWidget {
 }
 
 class _JobbRaktarSvgState extends State<JobbRaktarSvg> {
-  // Különböző részek állapota (színváltoztatás)
-  Map<String, Color> sectionColors = {
-    "balPolc": Colors.red,
-    "jobbPolc": Colors.red,
-    "balRekesz1": Colors.brown,
-    "balRekesz2": Colors.brown,
-    "jobbRekesz1": Colors.brown,
-    "jobbRekesz2": Colors.brown,
-  };
-
-  // Kattintás kezelése
-  void handleTap(String section) {
-    setState(() {
-      // A szürke színre változtatjuk
-      sectionColors[section] = Colors.grey;
-    });
-  }
+  String? _selectedId;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: defaultBackgroundColor,
-      appBar: AppBar(title: Text('Interactive SVG')),
-      body: Center(
-        child: Stack(
-          children: [
-            // SVG háttér
-            SvgPicture.string(
-              '''
-              <svg width="300" height="400" xmlns="http://www.w3.org/2000/svg">
-                <!-- Háttér (Szoba) -->
-                <rect x="0" y="0" width="300" height="400" stroke="navy" stroke-width="2" fill="navy" />
-                <!-- Bal oldali egybefüggő polc -->
-                <rect x="20" y="20" width="50" height="350" stroke="red" stroke-width="2" fill="${sectionColors["balPolc"]}" id="balPolc"/>
-                <!-- Jobb oldali egybefüggő polc -->
-                <rect x="230" y="20" width="50" height="350" stroke="red" stroke-width="2" fill="${sectionColors["jobbPolc"]}" id="jobbPolc"/>
-                <!-- Bal oldali rekeszek -->
-                <rect x="30" y="30" width="30" height="60" stroke="gold" stroke-width="2" fill="${sectionColors["balRekesz1"]}" id="balRekesz1"/>
-                <rect x="30" y="120" width="30" height="60" stroke="gold" stroke-width="2" fill="${sectionColors["balRekesz2"]}" id="balRekesz2"/>
-                <!-- Jobb oldali rekeszek -->
-                <rect x="240" y="30" width="30" height="60" stroke="gold" stroke-width="2" fill="${sectionColors["jobbRekesz1"]}" id="jobbRekesz1"/>
-                <rect x="240" y="120" width="30" height="60" stroke="gold" stroke-width="2" fill="${sectionColors["jobbRekesz2"]}" id="jobbRekesz2"/>
-                <!-- Tükrözött ajtó (negyedkör, befelé nyíló) -->
-                <path d="M 150 380 A 40 40 0 0 1 110 340" stroke="white" stroke-width="4" fill="none" />
-                <!-- Ajtó szárnya -->
-                <path d="M 150 380 L 150 340" stroke="white" stroke-width="4" />
-              </svg>
-              ''',
-              alignment: Alignment.center,
-              allowDrawingOutsideViewBox: true,
-            ),
-            // Kattintható bal polc
-            Positioned(
-              left: 20,
-              top: 20,
-              child: GestureDetector(
-                onTap: () => handleTap('balPolc'),
-                child: Container(
-                  width: 50,
-                  height: 350,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-            // Kattintható jobb polc
-            Positioned(
-              left: 230,
-              top: 20,
-              child: GestureDetector(
-                onTap: () => handleTap('jobbPolc'),
-                child: Container(
-                  width: 50,
-                  height: 350,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-            // Kattintható bal rekeszek
-            Positioned(
-              left: 30,
-              top: 30,
-              child: GestureDetector(
-                onTap: () => handleTap('balRekesz1'),
-                child: Container(
-                  width: 30,
-                  height: 60,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-            Positioned(
-              left: 30,
-              top: 120,
-              child: GestureDetector(
-                onTap: () => handleTap('balRekesz2'),
-                child: Container(
-                  width: 30,
-                  height: 60,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-            // Kattintható jobb rekeszek
-            Positioned(
-              left: 240,
-              top: 30,
-              child: GestureDetector(
-                onTap: () => handleTap('jobbRekesz1'),
-                child: Container(
-                  width: 30,
-                  height: 60,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-            Positioned(
-              left: 240,
-              top: 120,
-              child: GestureDetector(
-                onTap: () => handleTap('jobbRekesz2'),
-                child: Container(
-                  width: 30,
-                  height: 60,
-                  color: Colors.transparent,  // Átlátszó, de mégis érzékeli a kattintást
-                ),
-              ),
-            ),
-          ],
+    return Stack(
+      children: [
+        SvgPicture.string(
+          _buildSvg(),
+          fit: BoxFit.contain,
         ),
-      ),
+        Positioned.fill(
+          child: GestureDetector(
+            onTapUp: (details) {
+              _handleTap(details.localPosition);
+            },
+            child: Container(color: Colors.transparent),
+          ),
+        ),
+      ],
     );
+  }
+
+  String _buildSvg() {
+    return '''
+    <svg
+      width="100%" height="100%" viewBox="0 0 210 297"
+      version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <g transform="translate(-1.1193467,4.2908295)">
+        <rect style="fill:#808080" width="165" height="240" x="22.5" y="25"/>
+        <rect id="rect2" style="fill:${_selectedId == 'rect2' ? '#0000FF' : '#e6e6e6'}" width="45" height="230" x="25" y="30"/>
+        <rect id="rect3" style="fill:${_selectedId == 'rect3' ? '#0000FF' : '#e6e6e6'}" width="45" height="230" x="140" y="30"/>
+        <rect id="rect4" style="fill:${_selectedId == 'rect4' ? '#0000FF' : '#808080'}" width="38" height="48" x="28" y="35"/>
+        <rect id="rect5" style="fill:${_selectedId == 'rect5' ? '#0000FF' : '#808080'}" width="38" height="48" x="28" y="90"/>
+        <rect id="rect6" style="fill:${_selectedId == 'rect6' ? '#0000FF' : '#808080'}" width="38" height="48" x="28" y="145"/>
+        <rect id="rect7" style="fill:${_selectedId == 'rect7' ? '#0000FF' : '#808080'}" width="38" height="48" x="28" y="200"/>
+        <rect id="rect8" style="fill:${_selectedId == 'rect8' ? '#0000FF' : '#808080'}" width="38" height="48" x="144" y="35"/>
+        <rect id="rect9" style="fill:${_selectedId == 'rect9' ? '#0000FF' : '#808080'}" width="38" height="48" x="144" y="90"/>
+        <rect id="rect10" style="fill:${_selectedId == 'rect10' ? '#0000FF' : '#808080'}" width="38" height="48" x="144" y="145"/>
+        <rect id="rect11" style="fill:${_selectedId == 'rect11' ? '#0000FF' : '#808080'}" width="38" height="48" x="144" y="200"/>
+      </g>
+    </svg>
+    ''';
+  }
+
+  void _handleTap(Offset position) {
+    // Approximate touch regions for rectangles
+    if (_isWithin(position, 25, 30, 45, 230)) {
+      _onRectTapped('rect2');
+    } else if (_isWithin(position, 140, 30, 45, 230)) {
+      _onRectTapped('rect3');
+    } else if (_isWithin(position, 28, 35, 38, 48)) {
+      _onRectTapped('rect4');
+    } else if (_isWithin(position, 28, 90, 38, 48)) {
+      _onRectTapped('rect5');
+    } else if (_isWithin(position, 28, 145, 38, 48)) {
+      _onRectTapped('rect6');
+    } else if (_isWithin(position, 28, 200, 38, 48)) {
+      _onRectTapped('rect7');
+    } else if (_isWithin(position, 144, 35, 38, 48)) {
+      _onRectTapped('rect8');
+    } else if (_isWithin(position, 144, 90, 38, 48)) {
+      _onRectTapped('rect9');
+    } else if (_isWithin(position, 144, 145, 38, 48)) {
+      _onRectTapped('rect10');
+    } else if (_isWithin(position, 144, 200, 38, 48)) {
+      _onRectTapped('rect11');
+    }
+  }
+
+  bool _isWithin(Offset tap, double x, double y, double width, double height) {
+    return tap.dx >= x && tap.dx <= x + width && tap.dy >= y && tap.dy <= y + height;
+  }
+
+  void _onRectTapped(String id) {
+    setState(() {
+      _selectedId = id;
+    });
   }
 }
